94dc3fc50ec28401bb0a2449e46ac2a6f2e6acfc
Benjamin Congdon <bencongdon96@gmail.com>
2018-03-29 19:04:42 -0500

    Remove index from source control; Add list id to index key

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -9,24 +9,31 @@ from whoosh.qparser import QueryParser
 # Setup Index
 schema = Schema(id=ID(stored=True), content=TEXT)
 if not os.path.exists("index"):
-	os.mkdir("index")
+    os.mkdir("index")
 idx = create_in("index", schema)
 writer = idx.writer()
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 # Establish SQLite Connection
 conn = sqlite3.connect("../scraper.db")
-conn.row_factory = lambda cursor, row: {"id": str(row[0]), "content": str(row[1])}
+conn.row_factory = dict_factory
 c = conn.cursor()
 
 # Query for Documents
-c.execute('SELECT message_id, text from message')
+c.execute('SELECT message_id, list_id, text from message')
 entries = c.fetchall()
 for entry in entries:
-	writer.add_document(id=entry["id"], content=entry["content"])
+    doc_id = "{}-{}".format(entry["list_id"], entry["message_id"])
+    writer.add_document(id=doc_id, content=entry["text"])
 writer.commit()
 
 # Search for Documents
 with idx.searcher() as searcher:
-	query = QueryParser("content", idx.schema).parse('document')
-	results = searcher.search(query)
-	print(results[0])
+    query = QueryParser("content", idx.schema).parse('python')
+    results = searcher.search(query)
+    print(list(results))
